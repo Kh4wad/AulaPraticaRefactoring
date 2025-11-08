cbd6bf457235b3d55bbcd583601b137dd12cd6ab
Kh4wad <khawad@minha.fag.edu.br>
2025-11-07 23:25:06 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,17 +1,17 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // show figures for each rental
-            result += each.getMovie().getTitle() + ": " +
-                    String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-        // add footer lines
-        result += "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+    @Override
+    protected String header(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+
+    @Override
+    protected String eachLine(Rental aRental) {
+        return aRental.getMovie().getTitle() + ": " + String.valueOf(aRental.getCharge()) + "<BR>\n";
+    }
+
+    @Override
+    protected String footer(Customer aCustomer) {
+        String result = "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
         result += "On this rental you earned <EM>" +
                 String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
                 "</EM> frequent renter points<P>";
--- Statement.java
+++ Statement.java
@@ -1,4 +1,18 @@
-public abstract class Statement {
-	public abstract String value(Customer aCustomer);
+import java.util.Enumeration;
 
+public abstract class Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = header(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += eachLine(each);
+        }
+        result += footer(aCustomer);
+        return result;
+    }
+
+    protected abstract String header(Customer aCustomer);
+    protected abstract String eachLine(Rental aRental);
+    protected abstract String footer(Customer aCustomer);
 }
